336fc2d27bbf06dacef98c2dae10b90ba7c46c42
Mufaddhal <zaidan.mufaddhal1606@gmail.com>
2025-05-24 23:08:58 +0700

    Add files via upload

---
+++ React-Native-Job-Search/hook/useFetch.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            'x-rapidapi-key': 'a78bfe47d4msh47202c7a5e52fbdp14ef52jsn0fd538996d9e',
+            'x-rapidapi-host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query },
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+        try {
+            const response = await axios.request(options);
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert('There is an error bro');
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch };
+}
+
+export default useFetch;
